f2febbfc99fda0058830a833353e4005aede9207
Kaye <113017668+valenciakirst@users.noreply.github.com>
2024-08-24 11:34:55 +0000

    html edits

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP PRACTICAL EXAM</h1>
 <?php
 
 include('functions.php');
@@ -16,3 +25,6 @@ echo greetingMessage($name);
     <input type = "submit" value = "Submit">
 </form>
 
+
+</body>
+</html>
